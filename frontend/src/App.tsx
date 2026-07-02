ce8ac1df241a1ceec970b623414ca71d3c94c1a3
NZMia <mia7zwy@hotmail.com>
2026-07-02 17:30:26 +1200

    Replace frontend submodule with actual files

---
+++ frontend/src/App.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import "./App.css";
+
+type Todo = {
+  id: number;
+  title: string;
+};
+
+const todos: Todo[] = [
+  { id: 1, title: "Create frontend Dockerfile" },
+  { id: 2, title: "Create backend API" },
+  { id: 3, title: "Deploy services to AWS ECS" },
+];
+
+function App() {
+  return (
+    <main className="app">
+      <section className="card">
+        <h1>To-Do List Application</h1>
+        <p className="subtitle">Frontend service running in React + TypeScript</p>
+
+        <ul>
+          {todos.map((todo) => (
+            <li key={todo.id}>{todo.title}</li>
+          ))}
+        </ul>
+      </section>
+    </main>
+  );
+}
+
+export default App;
